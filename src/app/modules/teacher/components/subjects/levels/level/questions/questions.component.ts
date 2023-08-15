459176318b18c9915fa6f88b8cd9dae068d527a8
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2023-08-15 13:31:24 +0300

    question form fields without add form

--- src/app/modules/teacher/components/subjects/levels/level/questions/questions.component.ts
+++ src/app/modules/teacher/components/subjects/levels/level/questions/questions.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormGroup,
   FormControl,
@@ -14,8 +14,16 @@ import { Observable, Observer } from 'rxjs';
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css'],
 })
-export class QuestionsComponent {
+export class QuestionsComponent implements OnInit {
   validateForm!: FormGroup;
+
+  ngOnInit(): void {
+    (<FormArray>this.validateForm.get('wrongAnswers')).push(
+      new FormGroup({
+        wrongAnswer: new FormControl('', Validators.required),
+      })
+    );
+  }
 
   resetForm(e: MouseEvent): void {
     e.preventDefault();
@@ -27,34 +35,6 @@ export class QuestionsComponent {
       }
     }
   }
-
-  // addField(e?: MouseEvent): void {
-  //   if (e) {
-  //     e.preventDefault();
-  //   }
-  //   const id = this.listOfControl.length > 0 ? this.listOfControl[this.listOfControl.length - 1].id + 1 : 0;
-
-  //   const control = {
-  //     id,
-  //     controlInstance: `passenger${id}`
-  //   };
-  //   const index = this.listOfControl.push(control);
-  //   console.log(this.listOfControl[this.listOfControl.length - 1]);
-  //   this.validateForm.addControl(
-  //     this.listOfControl[index - 1].controlInstance,
-  //     new FormControl(null, Validators.required)
-  //   );
-  // }
-
-  // removeField(i: { id: number; controlInstance: string }, e: MouseEvent): void {
-  //   e.preventDefault();
-  //   if (this.listOfControl.length > 1) {
-  //     const index = this.listOfControl.indexOf(i);
-  //     this.listOfControl.splice(index, 1);
-  //     console.log(this.listOfControl);
-  //     this.validateForm.removeControl(i.controlInstance);
-  //   }
-  // }
 
   getControls() {
     return (this.validateForm.get('wrongAnswers') as FormArray).controls;
